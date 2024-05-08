5c1f25e15aca158118537b14eab519b6a5b0e03a
Suraj <surajlohit42@gmail.com>
2024-05-09 05:03:41 +0530

    Made the 3d Characters to render its working properly now!

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/character', function(req, res, next) {
+  res.render('characters');
+});
 
 module.exports = router;
